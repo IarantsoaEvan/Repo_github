1ccf2630ea2d23a8201e356febfd3cb301689ee6
Iarantsoa Evan Rabesandratana <irabesan@c1r12s3.42antananarivo.mg>
2024-06-09 13:39:26 +0300

    minitalk_01

---
+++ minitalk/client.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: irabesan <irabesan@student.42antanana      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/05 11:10:30 by irabesan          #+#    #+#             */
+/*   Updated: 2024/06/05 11:10:31 by irabesan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+void	ascii_to_bin(int pid, char *m_str)
+{
+	int	idx;
+	int	byte;
+
+	idx = 0;
+	while (m_str[idx])
+	{
+		byte = 0;
+		while (byte++ < 8)
+		{
+			if (((unsigned char)(m_str[idx]) >> (7 - i) & 1) == 1)
+				kill(pid, SIGUSR1);
+			else if (((unsigned char)(m_str[idx]) >> (7 - i) & 1) == 0)
+				kill(pid, SIGUSR2);
+		}
+	}
+	i = 0;
+	
+}
+int	main(int ac, char **av)
+{
+	char	*m_str;
+	int	ids;
+
+	if (ac == 3)
+	{
+		ids = ft_atoi(av[1]);
+		if (!ids)
+		{
+			ft_print("invalid PID\n");
+			return (0);
+		}
+		m_str = av[2];
+		if (m_str[0] == 0)
+		{
+			ft_printf("empty message\n");
+			return (0);
+		}
+		ascii_to_bin(ids, m_str);
+	}
+	else
+	{
+		ft_printf("Invalid numbers of arguments\n");
+		ft_printf("You must enter the PID then the message\n");
+	}
+	
+}
+
+  >> 1& 1
+
+000000011
---
+++ minitalk/minitalk.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: irabesan <irabesan@student.42antanana      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/09 10:54:46 by irabesan          #+#    #+#             */
+/*   Updated: 2024/06/09 10:54:47 by irabesan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif
